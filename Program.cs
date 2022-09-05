358d50a9afee862d30dc03386f0c9b5e75edeae9
Han <64648542+Han-Gigabyte@users.noreply.github.com>
2022-09-06 00:36:05 +0900

    Solved test
    hello

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Hello
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string[] ss = s.Split();
+            int a = int.Parse(ss[0]);
+            int b = int.Parse(ss[1]);
+            if(b-45<0)
+            {
+                if(a-1<0)
+                {
+                    Console.WriteLine("{0} {1}", 23, b + 15);
+                }
+                else
+                {
+                    Console.WriteLine("{0} {1}", a - 1, b + 15);
+                }
+            }
+            else
+            {
+                Console.WriteLine("{0} {1}", a, b - 45);
+            }
+        }
+
+
+    }
+}
